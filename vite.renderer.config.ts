28893e5946aeed0a30c84684f41cc10b3130ce76
YougLin <YougLin@proton.me>
2025-09-23 23:29:48 +0800

    feat: integrate Tailwind CSS

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   root: resolve(__dirname, 'src/renderer'),
-  plugins: [svelte()],
+  plugins: [tailwindcss(), svelte()],
   build: {
     outDir: resolve(__dirname, '.vite/build/renderer/main_window')
   }
