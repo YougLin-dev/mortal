75ad2799525facd5babdc98cccb6196b76736ab6
YougLin <YougLin@proton.me>
2025-10-03 02:38:05 +0800

    fix: properly align titlebar with window controls area

--- src/renderer/lib/components/titlebar/titlebar.svelte
+++ src/renderer/lib/components/titlebar/titlebar.svelte
@@ -3,7 +3,7 @@
   import { ThemeSwitch, PinSwitch } from '$lib/components/ui/switch';
 </script>
 
-<header class="fixed top-0 right-0 left-0 z-50 flex h-titlebar-h w-titlebar-w items-center" style="app-region: drag;">
+<header class="fixed top-titlebar-area-y right-0 left-titlebar-area-x z-50 flex h-titlebar-h w-titlebar-w items-center" style="app-region: drag;">
   <TabBar class="w-[calc(var(--spacing-titlebar-w)-82px)]" />
   <PinSwitch class="mr-2" style="app-region: no-drag;" />
   <ThemeSwitch class="mr-auto" style="app-region: no-drag;" />
